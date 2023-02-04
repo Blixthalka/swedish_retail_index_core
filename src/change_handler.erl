ea88e8d44b956fc5fb88d12a33fa2262f7d844c7
Emil Nilsson <emil@sharpfin.com>
2023-02-04 10:25:51 +0100

    change and group by month for owners

---
+++ src/change_handler.erl
@@ -0,0 +1,26 @@
+-module(change_handler).
+
+-export([init/2]).
+
+init(Req, State) ->
+    case cowboy_req:method(Req) of
+        <<"GET">> ->
+            get(Req, State);
+        _ ->
+            Req1 = cowboy_req:reply(404, #{}, Req),
+            {ok, Req1, State}
+    end.
+
+get(Req, State) ->
+    {Old, New} = helper:member_diff(<<"2023-01-02">>, date_util:today()),
+
+    Ejson = {[
+        {old, Old},
+        {new, New}
+    ]},
+
+    Json = jiffy:encode(Ejson),
+    Req1 = cowboy_req:reply(200, #{<<"content-type">> => <<"application/json">>}, Json, Req),
+    {ok, Req1, State}.
+
+
